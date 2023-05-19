8c347dbf900cc8dae87ab1947ba194f2ca7a7b7e
Luis Rodriguez <luis_rdz5@hotmail.com>
2023-05-19 12:01:26 -0600

    se agrego validacion de rows en pandas

--- productos.py
+++ productos.py
@@ -2,14 +2,15 @@ import os
 from dotenv import load_dotenv
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
 import pandas as pd
 import psycopg2
 
 # Cargar variables de entorno desde el archivo .env
 load_dotenv()
 
-# Obtener credenciales de la cuenta de servicio desde las variables de entorno
-creds = service_account.Credentials.from_service_account_info({
+json_credenciales = {
     "type": os.environ["TYPE"],
     "project_id": os.environ["PROJECT_ID"],
     "private_key_id": os.environ["PRIVATE_KEY_ID"],
@@ -20,7 +21,17 @@ creds = service_account.Credentials.from_service_account_info({
     "token_uri": os.environ["TOKEN_URI"],
     "auth_provider_x509_cert_url": os.environ["AUTH_PROVIDER_X509_CERT_URL"],
     "client_x509_cert_url": os.environ["CLIENT_X509_CERT_URL"]
-})
+}
+
+
+
+# Obtener credenciales de la cuenta de servicio desde las variables de entorno
+creds = service_account.Credentials.from_service_account_info(json_credenciales)
+
+scope = ["https://spreadsheets.google.com/feeds",
+         "https://www.googleapis.com/auth/drive"]
+credsSheets = ServiceAccountCredentials.from_json_keyfile_dict(json_credenciales, scope)
+client = gspread.authorize(creds)
 
 folder_id = os.environ["FOLDER_PRODUCTS_ID"]
 
@@ -36,19 +47,101 @@ conn = psycopg2.connect(
 )
 
 
+products_catalog_str= [
+    {
+        "name": "PROCESADOR para csv",
+        "model": "BasesBocina",
+        "table":"Procesadores",
+    },{
+        "name": "AMPLIFICADORES para csv",
+        "model": "BasesBocina",
+        "table":"Amplificadores",
+    },{
+        "name": "Estereos (2022) para csv",
+        "model": "BasesBocina",
+        "table":"Estereos",
+    },{
+        "name": "ACCESORIOS DE INSTALACIÓN para csv",
+        "model": "BasesBocina",
+        "table":"Accesorios",
+    },{
+        "name": "BOCINAS para csv",
+        "model": "BasesBocina",
+        "table":"Bocinas",
+    },{
+        "name": "EPICENTROS para csv",
+        "model": "BasesBocina",
+        "table":"Epicentros",
+    },{
+        "name": "TWEETERS para csv (2022)",
+        "model": "BasesBocina",
+        "table":"Tweeters",
+    },{
+        "name": "BASES para csv",
+        "model": "BasesBocina",
+        "table":"Bases",
+    },{
+        "name": "BASES PARA BOCINA para csv",
+        "model": "BasesBocina",
+        "table":"basesBocina",
+    },{
+        "name": "SET DE MEDIOS para csv",
+        "model": "BasesBocina",
+        "table":"SetMedios",
+    },{
+        "name": "ADAPTADORES DE IMPEDANCIA para csv",
+        "model": "BasesBocina",
+        "table":"AdaptadoresImpedancia",
+    },{
+        "name": "ADAPTADORES ANTENA para csv",
+        "model": "BasesBocina",
+        "table":"AdaptadoresAntena",
+    },{
+        "name": "MEDIOS RANGOS para csv",
+        "model": "BasesBocina",
+        "table":"MediosRangos",
+    },{
+        "name": "WOOFERS para csv",
+        "model": "BasesBocina",
+        "table":"Woofers",
+    },{
+        "name": "CAJONES para csv",
+        "model": "BasesBocina",
+        "table":"Cajones",
+    },{
+        "name": "KIT DE CABLES para csv",
+        "model": "BasesBocina",
+        "table":"KitsCables",
+    },{
+        "name": "ARNESES para csv",
+        "model": "BasesBocina",
+        "table":"Arneses",
+    },{
+        "name": "ECUALIZADORES para csv",
+        "model": "BasesBocina",
+        "table":"Ecualizadores",
+    },
+                   
+]
+
+
+
+
+
+
+
+
+
 def get_files(folder_id):
     query = f"'{folder_id}' in parents and trashed = false"
     results = service.files().list(q=query, fields="nextPageToken, files(parents,name, id, mimeType)").execute()
     items = results.get("files", [])
     i=0
     data=[]
-    #print(f" recorriendo {folder_id}")
-    #print(f" recorriendo {items}")
     
     for item in items:
         if item["mimeType"] != "application/vnd.google-apps.folder":
             print(f" {i+1} .- {item['name']},  el nombre del id es: {item['id']}")
-            #filesArray[i][item["id"], item["name"]]
             file = {
                     'id':i+1,
                     'uuid':item["id"],
@@ -72,12 +165,84 @@ def validate_folder(id, folderobject):
     return response
 
 def validate_file(idFile, files):
-    file = get_file_object(idFile, files)
+    try:
+        file = get_file_object(idFile, files)
+        client = gspread.authorize(credsSheets)
+        #id_archivo=file["uuid"]
+        nombre_archivo=file["name"]
+        print(f'validando : {nombre_archivo}')        
+        fileSheet = client.open(nombre_archivo).get_worksheet(0)
+        #print(f'fileSheet es {fileSheet}')
+        #hoja_calculo = fileSheet.sheet1
+        tableSheet = get_table_sheet(nombre_archivo)
+        print (f'tablesheet es: {tableSheet["table"]}')
+        filas = fileSheet.get_all_values()
+        #print(f'filas es: {filas}')
+        df = pd.DataFrame(filas[1:], columns=filas[0]) 
+        validate = validar_dataframe(df, tableSheet["table"] )
+        if validate :
+            print (f"Ha sido correcta la validación del archivo {nombre_archivo} se ha validado contra la tabla {tableSheet['table']}")
+        else:
+            print (f"No ha sido correcta la validación del archivo {nombre_archivo} se ha validado contra la tabla {tableSheet['table']}")
+
+        return validate
+
+        #for index, fila in enumerate(filas):
+            # Realizar acciones con cada fila
+        #    if index != 0:
+        #        print(fila)
+    except (Exception) as error:
+        print(error)
+
+
     # prompt utilizado como lees archivos de google sheets en python para validar si pueden ser insertados en una bd postgresql
 
 
+def validar_dataframe(df, table ):
+    error = False
+    try:
+        # Realiza las validaciones en el DataFrame
+        # Puedes agregar aquí tu lógica de validación
+        cursor = conn.cursor()
+        # Ejemplo: Verifica si todas las columnas existen en la tabla de la base de datos
+        query= f"SELECT column_name FROM information_schema.columns WHERE table_name = '{table}'"
+        print(f"query es: {query}")
+        cursor.execute(query)
+        columnas_tabla = [registro[0] for registro in cursor.fetchall()]
+        #print(f"Se encontraron estas columnas en la tabla: {columnas_tabla}")
+
+        
+        # Realiza la validación fila por fila
+        for index, fila in df.iterrows():
+            # Verifica si cada columna existe en la tabla de la base de datos
+            print(f"validando el id {index} ")
+            columnas_faltantes = set(fila.index.str.lower()) - set(columnas_tabla)
+            if columnas_faltantes:
+                print("Las siguientes columnas faltan en la tabla de la base de datos:", columnas_faltantes)
+                error = True
+
+        # Si todas las validaciones pasan, retorna True
+        cursor.close()
+        conn.close()        
+        return not error
+    except (Exception) as error:
+        print(error)
+        return False
+
+
+
+
 def get_file_object(idFile, files):
-    print(f'Validar {idFile}')
+    for i in range(len(files)):
+        if files[i]['id'] == int(idFile):
+            return files[i]
+    return
+
+def get_table_sheet(nombre_archivo):
+    for i in range(len(products_catalog_str)):
+        if products_catalog_str[i]['name'] == nombre_archivo:
+            return products_catalog_str[i]
+    return
 
 
 def main():
